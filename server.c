99555b6434999c709d8048dc29868f7de92ddb48
panch013 <panch013@umn.edu>
2015-05-06 18:17:30 -0500

    Simulating Write-Read on 4 processors using Server-Client

--- server.c
+++ server.c
@@ -14,57 +14,79 @@ Always execute 'ipcrm shm <shmid>' before running the server
 main(int argc, char *argv[])
 {
     int c;
-    int shmid,i;
-    key_t key;
-    int *shm, *s;
+    int shmid_1, shmid_2, i;
+    key_t key_1, key_2;
+    int *shm_1, *shm_2, *s_1, *s_2;
 
-    int SHMSZ = atoi(argv[1]);
-    int LIMIT = atoi(argv[2]);
+    int SHMSZ_1 = atoi(argv[1]);
+    int LIMIT_1 = atoi(argv[2]);
+    int SHMSZ_2 = atoi(argv[3]);
+    int LIMIT_2 = atoi(argv[4]);
+    key_1   = atoi(argv[5]);
+    key_2   = atoi(argv[6]);
     /*
      * We'll name our shared memory segment
      * "5678".
+     * key_1 = 5678;
+     * key_2 = 5679;
      */
-    key = 5678;
 
     /*
      * Create the segment.
      */
 
-    printf("SIZE: %d\n", SHMSZ);
-    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) {
+    if ((shmid_1 = shmget(key_1, SHMSZ_1, IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
-
+    if ((shmid_2 = shmget(key_2, SHMSZ_2, IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
     /*
      * Now we attach the segment to our data space.
      */
-    if ((shm = shmat(shmid, NULL, 0)) == (int *) -1) {
+    if ((shm_1 = shmat(shmid_1, NULL, 0)) == (int *) -1) {
         perror("shmat");
         exit(1);
     }
-
+    if ((shm_2 = shmat(shmid_2, NULL, 0)) == (int *) -1) {
+        perror("shmat");
+        exit(1);
+    }
     /*
      * Now put some things into the memory for the
      * other process to read.
+     * First segment will have data [0-LIMIT_1]
+     * Second segment will have data [LIMIT_1-LIMIT_2]
+     * LIMIT_1 < LIMIT_2
      */
-    s = shm;
+    s_1 = shm_1;
 
-    printf("LIMIT: %d\n", LIMIT);
-    for (c = 0; c < LIMIT; c++){
-        *s = c;
+    for (c = 0; c < LIMIT_1; c++){
+        *s_1 = c;
         //printf("%d\t", *s);
-        s++;
+        s_1++;
     }
-    *s = LIMIT;
+    *s_1 = LIMIT_1;
 
+    s_2 = shm_2;
+
+    for (c = LIMIT_1; c < LIMIT_2; c++){
+        *s_2 = c;
+        //printf("%d\t", *s);
+        s_2++;
+    }
+    *s_2 = LIMIT_2;
     /*
      * Finally, we wait until the other process 
      * changes the first character of our memory
      * to '*', indicating that it has read what 
      * we put there.
      */
-    while (*shm != (LIMIT+1))
+    while (*shm_1 != (LIMIT_1+1))
+        sleep(1);
+    while (*shm_2 != (LIMIT_2+1))
         sleep(1);
 
     printf("Stopped!\n ");
